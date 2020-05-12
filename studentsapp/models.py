3f0d37c87172004c0863c80142cf6aa9fd30d9e3
lowenchi0509 <58016152+lowenchi0509@users.noreply.github.com>
2020-05-12 15:56:27 +0800

    Update models.py

--- studentsapp/models.py
+++ studentsapp/models.py
@@ -8,5 +8,20 @@ class student(models.Model):
 	cPhone = models.CharField(max_length=50, blank=True, default='')
 	cAddr = models.CharField(max_length=255,blank=True, default='')
 	
+
+class users(models.Model):
+    uid = models.CharField(max_length=50, null=False)
+    created_time = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.uid
+
+class booking(models.Model):
+    bid = models.CharField(max_length=50, default='0', null=False)
+    roomtype = models.CharField(max_length=20, null=False)
+    roomamount = models.CharField(max_length=5, null=False)
+    datein = models.CharField(max_length=20, null=False)
+    dateout = models.CharField(max_length=20, null=False)
+    
 	def __str__(self):
 		return self.cName
