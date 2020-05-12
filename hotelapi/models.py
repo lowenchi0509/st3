987fe23935c48733573b10db1b576e32d2671f47
lowenchi0509 <58016152+lowenchi0509@users.noreply.github.com>
2020-05-13 00:38:57 +0800

    Update models.py

--- hotelapi/models.py
+++ hotelapi/models.py
@@ -4,8 +4,8 @@ class users(models.Model):
     uid = models.CharField(max_length=50, null=False)
     created_time = models.DateTimeField(auto_now=True)
     
-def __str__(self):
-    return self.uid
+    def __str__(self):
+      return self.uid
 
 class booking(models.Model):
     bid = models.CharField(max_length=50, default='0', null=False)
@@ -14,5 +14,5 @@ class booking(models.Model):
     datein = models.CharField(max_length=20, null=False)
     dateout = models.CharField(max_length=20, null=False)
     
-def __str__(self):
-    return self.id
+    def __str__(self):
+      return self.id
