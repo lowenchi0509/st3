e501b2213bd8bca2709b9bac7707c8f999aaaba5
lowenchi0509 <58016152+lowenchi0509@users.noreply.github.com>
2020-05-13 00:52:56 +0800

    Update admin.py

--- studentsapp/admin.py
+++ studentsapp/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from studentsapp.models import student
 from formapi.models import users
-from formapi.models import users
+from hotelapi.models import booking,users
 class studentAdmin(admin.ModelAdmin):
     # 第三種方式，加入 ModelAdmin 類別，定義顯示欄位、欄位過濾資料、搜尋和排序
 	list_display=('id','cName','cSex','cBirthday','cEmail','cPhone','cAddr')
