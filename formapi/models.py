5eac1267c7ed86c7590009b60c1780193e782c4d
lowenchi0509 <58016152+lowenchi0509@users.noreply.github.com>
2020-05-12 23:32:56 +0800

    Update models.py

--- formapi/models.py
+++ formapi/models.py
@@ -4,5 +4,5 @@ class users(models.Model):
     uid = models.CharField(max_length=50, null=False)
     datatest = models.CharField(max_length=50, null=False)
     
-    def __str__(self):
-        return self.uid
+def __str__(self):
+    return self.uid
