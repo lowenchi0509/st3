4fd9ab678af4d2356952c85fe0c84913364b07fd
lowenchi0509 <58016152+lowenchi0509@users.noreply.github.com>
2020-05-12 23:59:07 +0800

    Delete admin.py

--- exform/formapi/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from formapi.models import users
-
-class usersAdmin(admin.ModelAdmin):
-    list_display=('uid','datatest')
-admin.site.register(users, usersAdmin)
