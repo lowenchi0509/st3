86ad4f3f899f2f96ede1ba569ad1c32995ef286c
lowenchi0509 <58016152+lowenchi0509@users.noreply.github.com>
2020-05-13 01:22:25 +0800

    Update admin.py

--- formapi/admin.py
+++ formapi/admin.py
@@ -2,10 +2,6 @@ from django.contrib import admin
 from formapi.models import users
 
 class usersAdmin(admin.ModelAdmin):
-    uid = models.CharField(max_length=50, null=False)
-    datatest = models.CharField(max_length=50, null=False)
-    
-def __str__(self):
-   return self.uid
-admin.site.register(users,usersAdmin)
+    list_display=('uid','datatest')
 
+admin.site.register(users,usersAdmin)
