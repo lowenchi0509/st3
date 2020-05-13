5e21fd42dd6a05b2c13cfad6b78a95a39bff160c
lowenchi0509 <58016152+lowenchi0509@users.noreply.github.com>
2020-05-13 09:22:36 +0800

    Update urls.py

--- students/urls.py
+++ students/urls.py
@@ -19,7 +19,7 @@ from django.conf.urls import url
 from studentsapp import views
 from django.urls import include
 #from formapi import views as v2
-from func1api import views as v3
+#from func1api import views as v3
 #from hotelapi import views as v4
 
 
@@ -35,7 +35,7 @@ urlpatterns = [
     url(r'^hello3/(\w+)/$',views.hello3),
     url(r'^callback/', include(('studentsapp.urls', 'studentsapp'), namespace='callback')),
    # url(r'^callback', v4.callback),
-    url(r'^callback', v3.callback),
+   # url(r'^callback', v3.callback),
    # url(r'^callback', v2.callback),
 ]
 
